ffb2c3d0225e6ecc1aff257068f7dcf0d968665a
Amol-sanap <132637167+Amol-sanap@users.noreply.github.com>
2023-05-05 11:01:17 +0530

    firsttask

---
+++ openCart/src/testpackage/TestClass.java
@@ -0,0 +1,14 @@
+package testpackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hi");
+		System.out.println("This is first project deploy on GitHub");
+		
+
+	}
+
+}
